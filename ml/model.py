dc6217b0aad979f9c66050b4b8774739eab2fe87
liz-2450 <este260@wgu.edu>
2024-06-29 22:14:49 -0600

    update model.py & train_model.py

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+from sklearn.tree import DecisionTreeClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,8 +19,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-   # TODO: implement the function
-    pass
+    model = DecisionTreeClassifier()
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +60,8 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    preds = model.predict(X)
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +73,13 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    with open(path, "wb") as output:
+        pickle.dump(model, output)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    with open(path, "rb") as input:
+        return pickle.load(input)
 
 
 def performance_on_categorical_slice(
@@ -118,11 +119,16 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
-    X_slice, y_slice, _, _ = process_data(
+    X_slice, y_slice, _, _ = process_data(data,
+                                          categorical_features=categorical_features,
+                                          label=label,
+                                          training=False,
+                                          encoder=encoder,
+                                          lb=lb)
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
-    )
-    preds = # your code here to get prediction on X_slice using the inference function
+
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
