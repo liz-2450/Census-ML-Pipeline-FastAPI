42d79b030dc64ae2e8971aa1ad898ae36aa2f59c
liz-2450 <este260@wgu.edu>
2024-07-04 12:21:55 -0600

    corrected implementation error of performance_on_categorical_slice

--- ml/model.py
+++ ml/model.py
@@ -118,8 +118,8 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
-    X_slice, y_slice, _, _ = process_data(data,
+    slice_data = data[data[column_name]==slice_value]
+    X_slice, y_slice, _, _ = process_data(slice_data,
                                           categorical_features=categorical_features,
                                           label=label,
                                           training=False,
